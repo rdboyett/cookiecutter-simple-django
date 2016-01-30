fa5977fa1e87d23555d58be513339a7fcd972faa
rboyett <dallas20>
2016-01-30 15:18:08 -0600

    changed readme and settings

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/local.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/local.py
@@ -8,6 +8,31 @@ ADMINS = (
 MANAGERS = ADMINS
 
 
-# EMAIL CONFIGURATION
+# Mail settings
 # ------------------------------------------------------------------------------
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# CACHING
+# ------------------------------------------------------------------------------
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
+    }
+}
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar', )
+
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
